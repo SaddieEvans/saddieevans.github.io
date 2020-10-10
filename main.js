f3c5b926d5285dd9b602241209a361daad2c03cf
Madzia <72132532+SaddieEvans@users.noreply.github.com>
2020-10-10 21:41:23 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,51 @@
+(function( window ){
+    window.watchResize = function( callback ){
+        var resizing;
+        callback.size = 0;
+        function done()
+        {
+            var curr_size = window.innerWidth;
+            clearTimeout( resizing );
+            resizing = null;
+            // only run on a true resize
+            if ( callback.size != curr_size )
+            {
+                callback();
+                callback.size = curr_size;
+            }
+        }
+        window.addEventListener('resize', function(){
+            if ( resizing )
+            {
+                clearTimeout( resizing );
+                resizing = null;
+            }
+            resizing = setTimeout( done, 50 );
+        });
+        // init
+        callback();
+    };
+}(window));
 
+if ( ! 'classList' in document.body ) { return; }
+
+    var $html = document.getElementsByTagName('html')[0],
+		page_classes = $html.classList,
+		$menu_opener = document.getElementById( 'nav-jump' ),
+		$menu_closer = document.getElementById( 'menu-close' ),
+		drawer_enabled_class = 'drawer-nav-enabled',
+		drawer_open_class = 'drawer-nav-open';
+
+	var toggleDrawerNav_running = false;
+	function toggleDrawerNav( event ) {
+		event.preventDefault();
+    if ( toggleDrawerNav_running ) {return}
+    toggleDrawerNav_running = true;
+    
+    page_classes.toggle( 'draver-nav-open' ) ; 
+    setTimeout(function(){
+      toggleDrawerNav_running = false; } , 500); }
+
+    window.watchResize(function(){
+      if(window.innerWidth<=648 & & ! page_classes.contains('drawer-nav-enabled') ){
+        else if (window.innerWidth>648 page_classes.contains('drawer-nav-enabled') ) } 
