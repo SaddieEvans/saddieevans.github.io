8eba0e34312762a79894ea5a9091b014fa4bb573
Madzia <72132532+SaddieEvans@users.noreply.github.com>
2020-10-11 16:39:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -28,3 +28,14 @@
 }(window));
 
 
+function openNav() {
+  document.getElementById("Sidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("Sidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.body.style.backgroundColor = "white";
+}
