b40089efa356029ff7b55d7c564e4fa0046f7300
Madzia <72132532+SaddieEvans@users.noreply.github.com>
2020-10-11 16:42:41 +0200

    Update main.js

--- main.js
+++ main.js
@@ -30,12 +30,10 @@
 
 function openNav() {
   document.getElementById("Sidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeNav() {
   document.getElementById("Sidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
   document.body.style.backgroundColor = "white";
 }
