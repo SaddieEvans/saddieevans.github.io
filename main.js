603025099a646f8bcc89bb5a351fb205e6cb8165
Madzia <72132532+SaddieEvans@users.noreply.github.com>
2020-10-11 16:55:40 +0200

    Update main.js

--- main.js
+++ main.js
@@ -26,3 +26,15 @@
         callback();
     };
 }(window));
+
+function openNav() {
+  document.getElementById("sidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("Sidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.body.style.backgroundColor = "white";
+}
