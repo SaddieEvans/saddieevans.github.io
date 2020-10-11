1749bbd3d99481990f5e4500ee0165a5cda31ed9
Madzia <72132532+SaddieEvans@users.noreply.github.com>
2020-10-11 15:35:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
+(function( window ){
+    window.watchResize = function( callback ){
+        var resizing;
+        callback.size = 0;
+        function done()
+        {
+            var curr_size = window.innerWidth;
+            clearTimeout( resizing );
+            resizing = null;
+            // only run on a true resize
+            if ( callback.size != curr_size )
+            {
+                callback();
+                callback.size = curr_size;
+            }
+        }
+        window.addEventListener('resize', function(){
+            if ( resizing )
+            {
+                clearTimeout( resizing );
+                resizing = null;
+            }
+            resizing = setTimeout( done, 50 );
+        });
+        // init
+        callback();
+    };
+}(window));
 
+function openNav() {
+  document.getElementById("Sidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("Sidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.body.style.backgroundColor = "white";
+}
